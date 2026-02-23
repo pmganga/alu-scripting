380577623d9c3a96faa1e0c63cc65fd4a0001b94
Your Name <you@example.com>
2026-02-23 10:32:12 -0800

    Fix W293 PEP8 whitespace errors in Task 0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -12,14 +12,14 @@ def number_of_subscribers(subreddit):
     If the subreddit is invalid, returns 0.
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    
+
     # Strictly formatted API User-Agent to comply with Reddit's rules
     headers = {"User-Agent": "python:alu.api.advanced:v1.0 (by /u/philip)"}
-    
+
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code != 200:
         return 0
-        
+
     data = response.json().get("data", {})
     return data.get("subscribers", 0)
